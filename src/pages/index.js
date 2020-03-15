97ff99e4d1b10062ac6b9f427cc1bf9c1d767085
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-03-15 13:08:15 -0700

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { FaAdobe } from "react-icons/fa"
 export default () => (
   <Layout>
     <div>hello from backroads</div>
+    <FaAdobe />
   </Layout>
 )

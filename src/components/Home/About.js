d2a21ecd3c27e9c3cdb5dac1453418d46920fb74
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-03-21 13:29:38 -0700

    update about us page

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,11 +1,27 @@
 import React from "react"
 import Title from "../Title"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
 export const About = () => {
   return (
-    <div>
-      hello from about
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div>
+            <img src={img} alt="about compant" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>lofdas fdsa fsdf lkasjdflkfjdsalk;jf w</p>
+          <p>lofdas fdsa fsdf lkasjdflkfjdsalk;jf w</p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 export default About

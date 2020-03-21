e320ad8b8d3c689ed6e21ebcd50c725c25902666
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-03-21 13:46:07 -0700

    add services section

---
+++ src/constants/services.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { FaWallet, FaTree, FaSocks } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: "saving money",
+    text: "asdfs adfsafads fas  fasdf asdf asdf sad fsda fasd",
+  },
+  {
+    icon: <FaTree />,
+    title: "endless hiking",
+    text: "asdfs adfsafads fas  fasdf asdf asdf sad fsda fasd",
+  },
+  {
+    icon: <FaSocks />,
+    title: "amazing comfort",
+    text: "asdfs adfsafads fas  fasdf asdf asdf sad fsda fasd",
+  },
+]

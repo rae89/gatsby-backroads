9aa9e6c3e8974d072f946e47a2e274ecb56a2ac1
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-03-21 13:19:47 -0700

    add styles

---
+++ src/components/Home/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Title from "../Title"
+export const About = () => {
+  return (
+    <div>
+      hello from about
+      <Title title="about" subtitle="us" />
+    </div>
+  )
+}
+export default About

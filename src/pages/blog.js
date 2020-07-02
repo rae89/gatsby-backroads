fb132e68f2f5621f35e43efbaa855bfeefe934a9
Robert  Espinoza <gravitywaves@Roberts-iMac-Pro.local>
2020-07-01 23:18:36 -0700

    gatsby-image fluid details

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-export const blog = ({ data }) => {
+import Images from "../examples/Images"
+const blog = () => {
   return (
     <Layout>
       hello from blog page<Link to="/">back home</Link>
-      <h1> title: {data.site.siteMetadata.title}</h1>
+      <Images />
     </Layout>
   )
 }
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
-        author
-        data {
-          age
-        }
-      }
-    }
-  }
-`
 export default blog
